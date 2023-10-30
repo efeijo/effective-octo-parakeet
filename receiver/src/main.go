7a8f1027c175bb1d5062a8afc7f7a91e45977b67
efeijo <emanuell.feijo@gmail.com>
2023-10-30 19:33:11 +0000

    makefile, basic deployment and services setup done, TODO: redis client host

--- receiver/src/main.go
+++ receiver/src/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"os"
 
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	var ctx = context.Background()
+
+	redisChan := os.Getenv("REDIS_CHANNEL")
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+
+	rdb := redis.NewClient(
+		&redis.Options{
+			Addr:     "localhost:6379",
+			Password: redisPassword,
+		},
+	)
+
+	fmt.Println(rdb.Ping(ctx).Result())
+
+	pubSub := rdb.Subscribe(ctx, redisChan)
+
+	pubSub.Receive(ctx)
+	for ev := range pubSub.Channel() {
+		switch ev {
+
+		}
+	}
 }
--- sender/src/main.go
+++ sender/src/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
+
 func main() {
+	ctx := context.Background()
+
+	redisChan := os.Getenv("REDIS_CHANNEL")
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+
+	rdb := redis.NewClient(
+		&redis.Options{
+			Addr:     "localhost:6379",
+			Password: redisPassword,
+		},
+	)
+
+	fmt.Println(rdb.Ping(ctx).Result())
+	err := rdb.Publish(ctx, redisChan, "msg").Err()
+
+	fmt.Println(err)
 
 }
